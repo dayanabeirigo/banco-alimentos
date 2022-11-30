54e843784aa908930940a3f5686051459cb0114e
Dayana Cecília Reis Beirigo Dutra <dayana.beirigo@gmail.com>
2022-11-29 23:07:46 -0300

    feat: adicionar model Beneficiario

--- banco-alimentos/Models/Beneficiario.cs
+++ banco-alimentos/Models/Beneficiario.cs
@@ -1,6 +1,22 @@
-﻿namespace banco_alimentos.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace banco_alimentos.Models
 {
+    [Table("Beneficiarios")]
     public class Beneficiario
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o CPF!")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório Informar o nome!")]
+        public string Nome { get; set; }
     }
 }
